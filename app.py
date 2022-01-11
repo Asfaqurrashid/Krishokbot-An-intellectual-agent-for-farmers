a41f5afa57628123c5896690fddc695880c24736
Md. Asfaqur Rashid <47357725+Asfaqurrashid@users.noreply.github.com>
2022-01-12 02:50:52 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -14,9 +14,10 @@ app = Flask(__name__)
 def index():
     return "hello_world"
     
-@app.route('/chatbot', methods=['POST'])
+@app.route('/chatbot', methods=["POST"])
 def chatbotResponse():
-    question = request.args.get('question')
+    question = str(request.args.get("question"))
+    question = str(question)
     response = chatbot_response.chatbot_response(question)
     return jsonify({"response": str(response)})
 
